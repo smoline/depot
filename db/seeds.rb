0dbf1343d659fea35ab6b61aca2b79e16ac55948
Sherilyn Moline <smoline@gmail.com>
2017-11-10 12:40:11 -0500

    products added, index edited

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,10 +2,28 @@ Product.delete_all
 #
 Product.create!(title: 'Seven Mobile Apps in Seven Weeks',
                 description:
-                  %{ <p>
-                    < em > Native Apps, Multiple Platforms </em>
-                    Answer the question “Can we build this for ALL the devices?” with a ​   resounding YES. This book will help you get there with a real-world ​   introduction to seven platforms, whether you're new to mobile or an experienced devloper needing to expand your options. Plus, you'll find out which cross-platform solutions make the most sense for your needs.
+                  %{ <p>Native Apps, Multiple Platforms. Answer the question “Can we build this for ALL the devices?” with a ​   resounding YES. This book will help you get there with a real-world ​   introduction to seven platforms, whether you're new to mobile or an experienced devloper needing to expand your options. Plus, you'll find out which cross-platform solutions make the most sense for your needs.
                     </p> },
                 image_url: '7apps.jpg',
                 price: 26.00)
 #
+Product.create!(title: 'Ruby Performance Optimization',
+                description:
+                  %{ <p>Native Apps, Multiple Platforms. Answer the question “Can we build this for ALL the devices?” with a ​   resounding YES. This book will help you get there with a real-world ​   introduction to seven platforms, whether you're new to mobile or an experienced devloper needing to expand your options. Plus, you'll find out which cross-platform solutions make the most sense for your needs.
+                    </p> },
+                image_url: 'adrpo.jpg',
+                price: 26.00)
+#
+Product.create!(title: 'Rails, Angular, Postgres and Bootstrap',
+                description:
+                  %{ <p>Native Apps, Multiple Platforms. Answer the question “Can we build this for ALL the devices?” with a ​   resounding YES. This book will help you get there with a real-world ​   introduction to seven platforms, whether you're new to mobile or an experienced devloper needing to expand your options. Plus, you'll find out which cross-platform solutions make the most sense for your needs.
+                    </p> },
+                image_url: 'dcbang.jpg',
+                price: 26.00)
+#
+Product.create!(title: 'Programming Ruby 1.9 & 2.0',
+                description:
+                  %{ <p>Native Apps, Multiple Platforms. Answer the question “Can we build this for ALL the devices?” with a ​   resounding YES. This book will help you get there with a real-world ​   introduction to seven platforms, whether you're new to mobile or an experienced devloper needing to expand your options. Plus, you'll find out which cross-platform solutions make the most sense for your needs.
+                    </p> },
+                image_url: 'ruby.jpg',
+                price: 26.00)
