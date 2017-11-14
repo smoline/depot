757b354ee9ea8ca393e978813f669b76ca771c47
Sherilyn Moline <smoline@gmail.com>
2017-11-14 16:09:59 -0500

    locale changed

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -9,6 +9,8 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
     get products_url
     assert_response :success
+    assert_select 'tr', minimum: 3
+    assert_select 'td.actions ul li', minimum: 9
   end
 
   test "should get new" do
